69d1c54c187cd1970e70b3395f63f79fb5750398
dfreerksen <dfreerksen@gmail.com>
2017-10-06 15:12:12 -0700

    Frontend pages

--- app/controllers/archangel/frontend/pages_controller.rb
+++ app/controllers/archangel/frontend/pages_controller.rb
@@ -3,7 +3,37 @@
 module Archangel
   module Frontend
     class PagesController < FrontendController
-      def show; end
+      before_action :set_resource, only: %i[show]
+
+      def show
+        if redirect_to_homepage?
+          return redirect_to root_path, status: :moved_permanently
+        end
+
+        respond_with @page
+      end
+
+      protected
+
+      def set_resource
+        page_path = params.fetch(:path, nil)
+
+        @page = page_path.blank? ? find_homepage : find_page(page_path)
+      end
+
+      def redirect_to_homepage?
+        return false unless page
+
+        page.homepage?
+      end
+
+      def find_homepage
+        Archangel::Page.published.homepage.first!
+      end
+
+      def find_page(path)
+        Archangel::Page.published.find_by!(path: path)
+      end
     end
   end
 end
