50c8080c87e56bf6683d661dbdeb21071abd233e
David Freerksen <dfreerksen@gmail.com>
2017-10-09 19:03:32 -0700

    Remove storage type from uploader

--- app/uploaders/archangel/application_uploader.rb
+++ app/uploaders/archangel/application_uploader.rb
@@ -4,9 +4,6 @@ module Archangel
   class ApplicationUploader < CarrierWave::Uploader::Base
     include CarrierWave::MiniMagick
 
-    storage :file
-    # storage :fog
-
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
