d2727c3df6ffde7aaddbbe4e392f27bb697b79be
dfreerksen <dfreerksen@gmail.com>
2017-10-27 13:55:24 -0700

    Add missing test files

---
+++ spec/policies/archangel/asset_policy_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module Archangel
+  RSpec.describe AssetPolicy, type: :policy do
+    subject { described_class.new(user, record) }
+
+    let(:user) { create(:user) }
+    let(:record) { create(:asset) }
+
+    it { should permit(:index) }
+    it { should permit(:show) }
+    it { should permit(:create) }
+    it { should permit(:new) }
+    it { should permit(:update) }
+    it { should permit(:edit) }
+    it { should permit(:destroy) }
+  end
+end
