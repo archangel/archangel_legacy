55cb1d21f676b026a70e3324251e7934a9d6a2be
dfreerksen <dfreerksen@gmail.com>
2017-10-27 13:55:56 -0700

    Rubocop cleaning

--- lib/generators/archangel/install/templates/config/initializers/devise.rb
+++ lib/generators/archangel/install/templates/config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` on Rails 4+ applications as its
   # `secret_key` by default. You can change it below and use your own
   # secret key.
-  config.secret_key = <%= SecureRandom.hex(50).inspect %>
+  config.secret_key = "<%= SecureRandom.hex(50).to_s %>"
 
   # Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
