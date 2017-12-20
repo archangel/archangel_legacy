ad5d0a7c590170bd2eb4324956496b69f2a51102
David Freerksen <dfreerksen@gmail.com>
2017-12-20 09:11:06 -0800

    Simple fixes

--- app/models/archangel/entry.rb
+++ app/models/archangel/entry.rb
@@ -6,9 +6,7 @@ module Archangel
 
     serialize :value, JSON
 
-    acts_as_list scope: :collection_id,
-                 top_of_list: 0,
-                 add_new_at: :bottom
+    acts_as_list scope: :collection_id, top_of_list: 0, add_new_at: :top
 
     validates :collection_id, presence: true
     validates :available_at, allow_blank: true, date: true
