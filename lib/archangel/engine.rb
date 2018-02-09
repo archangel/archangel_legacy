da412c50ffe4c07cd3da0d10bccdb2b5eed30709
David Freerksen <dfreerksen@gmail.com>
2018-02-08 21:56:33 -0800

    Rubocop fix

--- lib/archangel/engine.rb
+++ lib/archangel/engine.rb
@@ -37,7 +37,7 @@ module Archangel
         full_path = Pathname.new("#{path}/app/themes/*/assets/**/*")
 
         path_regex = %r{assets/(javascripts|stylesheets)}
-        file_regex = %r{(auth|backend|frontend).(js|css)}
+        file_regex = /(auth|backend|frontend)\.(js|css)/
         allowed_regex = %r{([^/]+)/#{path_regex}/([^/]+)/#{file_regex}}
 
         Dir.glob(full_path).each do |file|
