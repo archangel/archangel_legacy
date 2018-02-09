99f88720d4e741df2e09eeed1c169a532fdc4036
David Freerksen <dfreerksen@gmail.com>
2018-02-08 20:02:40 -0800

    Collection assign tag

--- lib/archangel/liquid/tags/collection_tag.rb
+++ lib/archangel/liquid/tags/collection_tag.rb
@@ -4,25 +4,72 @@ module Archangel
   module Liquid
     module Tags
       ##
-      # Collection custom tag for Liquid
+      # Assign sets a variable in your template.
       #
-      # Example
-      #   {% collection collection-name as articles %}
-      #     <div>{{ article.title }}</div>
-      #   {% endcollection %}
+      #   {% collection foo = 'my-collection' %}
       #
-      class CollectionTag < BaseTag
-        ##
-        # Render the Collection
-        #
-        # @param context [Object] the Liquid context
-        # @return [String] the Collection
-        #
+      # You can then use the variable later in the page.
+      #
+      #  {{ foo }}
+      #
+      class CollectionTag < ::Liquid::Tag
+        SYNTAX = /(#{::Liquid::VariableSignature}+)\s*=\s*(.*)\s*/om
+
+        def initialize(tag_name, markup, options)
+          super
+
+          if markup =~ SYNTAX
+            collection_name = ::Liquid::Variable.new($2, options).name
+
+            @to = $1
+            @from = override_collection(collection_name)
+          else
+            raise SyntaxError.new options[:locale].t("errors.syntax.assign")
+          end
+        end
+
         def render(context)
-          [
-            { title: "First item title" },
-            { title: "Second item title" }
-          ]
+          val = @from
+
+          context.scopes.last[@to] = val
+          context.resource_limits.assign_score += assign_score_of(val)
+
+          ""
+        end
+
+        def blank?
+          true
+        end
+
+        private
+
+        def override_collection(collection_slug)
+          items = load_collection_for(Archangel::Site.first, collection_slug)
+
+          items.each_with_object([]) do |item, collection|
+            collection << {
+              "id"           => item["id"],
+              "available_at" => item["available_at"]
+            }.reverse_merge(item["value"])
+          end
+        end
+
+        def load_collection_for(site, collection_slug)
+          collection = site.collections.find_by!(slug: collection_slug)
+
+          site.entries.where(collection: collection).map(&:attributes)
+        rescue StandardError
+          []
+        end
+
+        def assign_score_of(val)
+          if val.instance_of?(String)
+            val.length
+          elsif val.instance_of?(Array) || val.instance_of?(Hash)
+            val.inject(1) { |sum, i| sum + assign_score_of(i) }
+          else
+            1
+          end
         end
       end
     end
--- spec/lib/archangel/liquid/tags/collection_tag_spec.rb
+++ spec/lib/archangel/liquid/tags/collection_tag_spec.rb
@@ -5,7 +5,81 @@ require "rails_helper"
 module Archangel
   module Liquid
     module Tags
-      RSpec.describe CollectionTag do
+      RSpec.describe CollectionTag, type: :tag,
+                                    disable: :verify_partial_doubles do
+        let(:site) { create(:site) }
+
+        let(:context) do
+          ::Liquid::Context.new({ "site" => site }, {}, view: view)
+        end
+
+        it "returns nothing to the view" do
+          content = "{% collection things %}"
+
+          expect { ::Liquid::Template.parse(content).render(context) }.to(
+            raise_error(SyntaxError, /Syntax Error in 'assign'/)
+          )
+        end
+
+        it "returns nothing to the view" do
+          create(:collection, site: site, slug: "my-collection")
+
+          content = "{% collection things = 'my-collection' %}"
+
+          result = ::Liquid::Template.parse(content).render(context)
+
+          expect(result).to eq("")
+        end
+
+        it "returns collection content" do
+          collection = create(:collection, site: site, slug: "my-collection")
+          create(:field, :required, collection: collection, slug: "name")
+          create(:entry, collection: collection,
+                         value: { name: "First Person" })
+          create(:entry, collection: collection,
+                         value: { name: "Second Person" })
+
+          content = <<-LIQUID
+            {% collection things = 'my-collection' %}
+            {% for item in things %}
+              {{ forloop.index }}: {{ item.name }}
+            {% endfor %}
+          LIQUID
+
+          result = ::Liquid::Template.parse(content).render(context)
+
+          expect(result).to include("1: Second Person")
+          expect(result).to include("2: First Person")
+        end
+
+        it "returns nothing for empty collection" do
+          collection = create(:collection, site: site, slug: "my-collection")
+          create(:field, :required, collection: collection, slug: "name")
+
+          content = <<-LIQUID
+            {% collection things = 'my-collection' %}
+            {% for item in things %}
+              {{ item.name }}
+            {% endfor %}
+          LIQUID
+
+          result = ::Liquid::Template.parse(content).render(context)
+
+          expect(result).to eq("            \n            \n")
+        end
+
+        it "returns nothing for unknown collection" do
+          content = <<-LIQUID
+            {% collection things = 'unknown-collection' %}
+            {% for item in things %}
+              {{ item.name }}
+            {% endfor %}
+          LIQUID
+
+          result = ::Liquid::Template.parse(content).render(context)
+
+          expect(result).to eq("            \n            \n")
+        end
       end
     end
   end
