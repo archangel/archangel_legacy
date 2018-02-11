d79c69b5b03976bca3774f8e4823b14813ce6716
David Freerksen <dfreerksen@gmail.com>
2018-02-11 15:21:54 -0800

    Test without {{ content_for_layout }} in template

--- spec/services/archangel/template_render_service_spec.rb
+++ spec/services/archangel/template_render_service_spec.rb
@@ -6,25 +6,36 @@ module Archangel
   RSpec.describe TemplateRenderService, type: :service do
     context "#new" do
       it "builds the liquid content" do
-        template = create(:template, content: "~{{ content_for_layout }}~")
+        template = create(:template)
         content = "<p>This is the content of the page</p>"
 
         rendered = described_class.new(template,
                                        content_for_layout: content).call
 
-        expect(rendered).to include("~<p>This is the content of the page</p>~")
+        expect(rendered).to include("<p>This is the content of the page</p>")
       end
     end
 
     context ".call" do
       it "builds the liquid content" do
-        template = create(:template, content: "~{{ content_for_layout }}~")
+        template = create(:template)
         content = "<p>This is the content of the page</p>"
 
         rendered = described_class.call(template,
                                         content_for_layout: content)
 
-        expect(rendered).to include("~<p>This is the content of the page</p>~")
+        expect(rendered).to include("<p>This is the content of the page</p>")
+      end
+
+      it "builds the liquid content even without `{{ content_for_layout }}`" do
+        template = create(:template, content: "FULL TEMPLATE")
+        content = "<p>Content of the page is still rendered</p>"
+
+        rendered = described_class.call(template,
+                                        content_for_layout: content)
+
+        expect(rendered)
+          .to include("<p>Content of the page is still rendered</p>")
       end
     end
   end
