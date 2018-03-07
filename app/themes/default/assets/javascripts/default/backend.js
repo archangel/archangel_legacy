76b9f5e0fc620ca6ea5a026d8d18e77db57f38bd
David Freerksen <dfreerksen@gmail.com>
2018-03-06 21:52:49 -0800

    coreui

--- app/themes/default/assets/javascripts/default/backend.js
+++ app/themes/default/assets/javascripts/default/backend.js
@@ -6,29 +6,28 @@
 //= require rails-ujs
 //= require popper
 //= require bootstrap
-//= require moment
-//= require bootstrap-datetimepicker
-//= require selectize
-//= require summernote
-//= require cocoon
-//= require sortable.min
+// require moment
+// require bootstrap-datetimepicker
+// require selectize
+// require summernote
+// require cocoon
+// require sortable.min
 //
 //  Archangel helper scripts
-//= require "archangel/object"
-//= require "archangel/object/routes"
-//= require "archangel/object/routes/backend"
-//= require "archangel/object/translate"
-//= require "archangel/object/translate/datetime_picker"
-//= require "archangel/object/url"
-//= require "archangel/input/datetime_picker"
-//= require "archangel/input/meta_keywords"
-//= require "archangel/input/wysiwyg"
-//= require "archangel/input/parent/page"
-//= require "archangel/sortable"
-
+// require "archangel/object"
+// require "archangel/object/routes"
+// require "archangel/object/routes/backend"
+// require "archangel/object/translate"
+// require "archangel/object/translate/datetime_picker"
+// require "archangel/object/url"
+// require "archangel/input/datetime_picker"
+// require "archangel/input/meta_keywords"
+// require "archangel/input/wysiwyg"
+// require "archangel/input/parent/page"
+// require "archangel/sortable"
 //
 //  Third-party Archangel assets (from the vendor directory)
 //= require archangel/backend
 //
 //  Theme assets
-//= require_tree ./backend
+//= require default/backend/core
