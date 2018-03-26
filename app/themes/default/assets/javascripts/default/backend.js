c9c818be8c2f64e68114f5c4f50cb6dd033f5043
David Freerksen <dfreerksen@gmail.com>
2018-03-25 19:08:55 -0700

    Summernote and Selectize

--- app/themes/default/assets/javascripts/default/backend.js
+++ app/themes/default/assets/javascripts/default/backend.js
@@ -8,9 +8,9 @@
 //= require bootstrap
 // require moment
 // require bootstrap-datetimepicker
-// require selectize
-// require summernote
-// require cocoon
+//= require selectize
+//= require summernote
+//= require cocoon
 //= require sortable.min
 //
 //  Archangel helper scripts
@@ -23,8 +23,8 @@
 //= require "archangel/object/translate/sortable"
 //= require "archangel/object/url"
 // require "archangel/input/datetime_picker"
-// require "archangel/input/meta_keywords"
-// require "archangel/input/wysiwyg"
+//= require "archangel/input/meta_keywords"
+//= require "archangel/input/wysiwyg"
 //= require "archangel/input/parent/collection"
 //= require "archangel/input/parent/field"
 //= require "archangel/input/parent/page"
