c825687bd9cc2b33b1b425dd4213b2c231fcc51f
dfreerksen <dfreerksen@gmail.com>
2018-04-27 15:48:51 -0700

    Clean up and simplify Liquid view renderer

--- lib/archangel/liquid_view.rb
+++ lib/archangel/liquid_view.rb
@@ -33,15 +33,19 @@ module Archangel
     # @return [String] the rendered content
     #
     def render(template, local_assigns = {})
-      @view.controller.headers["Content-Type"] ||= "text/html; charset=utf-8"
+      default_controller.headers["Content-Type"] ||= "text/html; charset=utf-8"
 
       assigns = default_assigns(local_assigns)
-      options = { filters: default_filters, registers: default_registers }
+      options = { registers: default_registers }
 
       Archangel::RenderService.call(template, assigns, options)
     end
 
     protected
+
+    def default_controller
+      @view.controller
+    end
 
     def default_assigns(local_assigns)
       assigns = @view.assigns
@@ -53,40 +57,12 @@ module Archangel
       assigns.merge(local_assigns.stringify_keys)
     end
 
-    def default_controller
-      @view.controller
-    end
-
-    def default_filters
-      extra_filters = []
-
-      controller_class = default_controller.class
-
-      if controller_class.respond_to?(:liquid_filters)
-        controller_class.liquid_filters.each do |method|
-          extra_filters.merge! default_controller.send(method)
-        end
-      end
-
-      extra_filters
-    end
-
     def default_registers
-      extra_registers = {}
-
-      controller_class = default_controller.class
-
-      if controller_class.respond_to?(:liquid_registers)
-        controller_class.liquid_registers.each do |method|
-          extra_registers.merge! default_controller.send(method)
-        end
-      end
-
       {
         view: @view,
         controller: default_controller,
         helper: ActionController::Base.helpers
-      }.merge extra_registers
+      }
     end
   end
 end
