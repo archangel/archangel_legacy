d29106893061cc21ef09f8166b86631954193d01
David Freerksen <dfreerksen@gmail.com>
2018-05-09 22:56:24 -0700

    Add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ def prompt_for_admin_password
 end
 
 # Site
-site = Archangel::Site.first_or_create! do |item|
+site = Archangel::Site.first_or_create do |item|
   item.name = "Archangel"
   item.locale = "en"
 end
@@ -52,14 +52,92 @@ unless Archangel::User.first
     confirmed_at: Time.current
   }
 
-  Archangel::User.create!(attributes)
+  Archangel::User.create(attributes)
 end
 
 # Homepage
 Archangel::Page.published
-               .find_or_create_by!(site: site, homepage: true) do |item|
+               .find_or_create_by(site: site, homepage: true) do |item|
   item.slug = "homepage-#{Time.now.to_i}"
   item.title = "Welcome to Archangel"
-  item.content = "<p>Welcome to your new site.</p>"
+  item.content = %(
+    <p>Welcome to your new site.</p>
+  )
   item.published_at = Time.current
+end
+
+# Template
+page_template = Archangel::Template.find_or_create_by(
+  site: site,
+  partial: false
+) do |item|
+  item.name = "Example Page Template"
+  item.content = %(
+    <p>I think this is the beginning of a beautiful page template.</p>
+    {{ content_for_layout }}
+    <p>I think this is the end of a beautiful page template.</p>
+  )
+end
+
+# Page
+Archangel::Page.find_or_create_by(
+  site: site,
+  slug: "example-page",
+  template: page_template,
+  homepage: false
+) do |item|
+  item.title = "Example Page"
+  item.content = %(
+    <p>I think this is the content of the page.</p>
+  )
+  item.published_at = Time.now
+end
+
+# Template
+widget_template = Archangel::Template.find_or_create_by(
+  site: site,
+  partial: true
+) do |item|
+  item.name = "Example Widget Template"
+  item.content = %(
+    <p>I think this is the beginning of a beautiful widget template.</p>
+    {{ content_for_layout }}
+    <p>I think this is the end of a beautiful widget template.</p>
+  )
+end
+
+# Widget
+Archangel::Widget.find_or_create_by(
+  site: site,
+  slug: "example-widget",
+  template: widget_template
+) do |item|
+  item.name = "Example Widget"
+  item.content = %(
+    <p>I think this is the content of the widget.</p>
+  )
+end
+
+# Collection
+collection = Archangel::Collection.find_or_create_by(
+  site: site,
+  slug: "example-collection"
+) do |item|
+  item.name = "Example Collection"
+end
+
+# Fields
+Archangel::Field.find_or_create_by(
+  collection: collection,
+  slug: "field1",
+  required: true
+) do |item|
+  item.label = "Field 1"
+  item.classification = "string"
+end
+
+# Entry
+Archangel::Entry.find_or_create_by(collection: collection) do |item|
+  item.value = %({"field1":"Value for field 1"})
+  item.available_at = Time.now
 end
