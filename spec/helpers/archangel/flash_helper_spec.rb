85063b1de2e08629f9dde6a92b1d83a9c24ce981
David Freerksen <dfreerksen@gmail.com>
2018-05-13 17:50:07 -0700

    More flash helper tests

--- spec/helpers/archangel/flash_helper_spec.rb
+++ spec/helpers/archangel/flash_helper_spec.rb
@@ -9,20 +9,24 @@ module Archangel
         expect(helper.flash_class_for("success")).to eq("success")
       end
 
-      it "returns error class" do
+      it "returns `error` class" do
         expect(helper.flash_class_for("error")).to eq("danger")
       end
 
-      it "returns alert class" do
+      it "returns `alert` class" do
         expect(helper.flash_class_for("alert")).to eq("warning")
       end
 
-      it "returns notice class" do
+      it "returns `notice` class" do
         expect(helper.flash_class_for("notice")).to eq("info")
       end
 
       it "returns unknown class" do
         expect(helper.flash_class_for("unknown")).to eq("unknown")
+        expect(helper.flash_class_for("foo bar")).to eq("foo-bar")
+        expect(helper.flash_class_for("foo-bar")).to eq("foo-bar")
+        expect(helper.flash_class_for("foo_bar")).to eq("foo_bar")
+        expect(helper.flash_class_for("foo      bar")).to eq("foo-bar")
       end
     end
   end
