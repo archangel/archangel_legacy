e03e8845958ae7f31211e9f49dff91fe53518a71
David Freerksen <dfreerksen@gmail.com>
2018-05-14 20:49:03 -0700

    Frontend path not root path

--- spec/features/frontend/homepage_redirect_spec.rb
+++ spec/features/frontend/homepage_redirect_spec.rb
@@ -3,13 +3,13 @@
 require "rails_helper"
 
 RSpec.feature "Home page", type: :feature do
-  describe "when on home page slug" do
+  describe "when on home page path" do
     it "redirects to /" do
       home = create(:page, :homepage)
 
       visit archangel.frontend_page_path(home.path)
 
-      expect(page.current_path).to eq archangel.root_path
+      expect(page.current_path).to eq archangel.frontend_root_path
     end
   end
 end
