d4d4e47127336f0ba8143da1783953c36a3e2642
David Freerksen <dfreerksen@gmail.com>
2019-03-13 20:19:25 -0700

    Make datetimepicker more like the other inputs

--- app/assets/javascripts/archangel/input/datetimepicker.js
+++ app/assets/javascripts/archangel/input/datetimepicker.js
@@ -1,3 +1,25 @@
+$.fn.inputTimePicker = function () {
+  'use strict';
+};
+
+$.fn.inputDatePicker = function () {
+  'use strict';
+};
+
+$.fn.inputDateTimePicker = function () {
+  'use strict';
+};
+
 $(function() {
   'use strict';
+
+  // Time picker
+  $('input.time_picker').inputTimePicker();
+
+  // Date picker
+  $('input.date_picker').inputDatePicker();
+
+  // Date time picker
+  $('input.date_time_picker').inputDateTimePicker();
+
 });
