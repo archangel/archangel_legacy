4626f322c5c4fb1ac21bd3b21675360565e09452
David Freerksen <dfreerksen@gmail.com>
2019-04-16 14:28:17 -0700

    Remove unused helpers

--- app/helpers/archangel/auth_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Archangel
-  ##
-  # Authentication helpers
-  #
-  module AuthHelper
-  end
-end
--- app/helpers/archangel/frontend_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Archangel
-  ##
-  # Frontend helpers
-  #
-  module FrontendHelper
-  end
-end
