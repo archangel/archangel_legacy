ee9d88a9ff17ee0a66228cef0b8e809ef4d2a0e3
David Freerksen <dfreerksen@gmail.com>
2019-04-20 16:38:29 -0700

    Test redirect when homepage is requested

--- spec/requests/frontend/page_spec.rb
+++ spec/requests/frontend/page_spec.rb
@@ -4,14 +4,22 @@ require "rails_helper"
 
 RSpec.describe "Frontend - Page", type: :request do
   describe "with available page" do
-    let(:resource) { create(:page, slug: "foo") }
-
-    before { resource }
+    let!(:resource) { create(:page, slug: "foo") }
 
     it "returns successfully" do
       get "/foo"
 
       expect(response).to be_successful
+    end
+  end
+
+  describe "with homepage" do
+    let!(:resource) { create(:page, :homepage, slug: "foo") }
+
+    it "redirects to root path" do
+      get "/foo"
+
+      expect(response).to redirect_to("/")
     end
   end
 
