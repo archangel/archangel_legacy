7ac3e0d41ef93e71666e891e5d32d29e35359266
David Freerksen <dfreerksen@gmail.com>
2019-04-28 13:32:56 -0700

    Separate Page metatag tests

--- spec/features/backend/pages/create_spec.rb
+++ spec/features/backend/pages/create_spec.rb
@@ -61,26 +61,6 @@ RSpec.feature "Backend - Pages (HTML)", type: :feature do
         expect(page).to have_content("Page was successfully created.")
       end
 
-      scenario "with valid data including meta tags" do
-        visit "/backend/pages/new"
-
-        fill_in "Title", with: "Amazing Page"
-        fill_in "Slug", with: "amazing"
-
-        page.find("textarea#page_content").set("<p>Content of the page</p>")
-
-        fill_in "Published At", with: Time.now
-
-        within ".form-group.page_metatags" do
-          fill_in "Name", with: "description"
-          fill_in "Content", with: "Description of the Page"
-        end
-
-        click_button "Create Page"
-
-        expect(page).to have_content("Page was successfully created.")
-      end
-
       scenario "with duplicate slug at different levels" do
         create(:page, slug: "amazing", title: "Amazing")
         create(:page, slug: "grace")
