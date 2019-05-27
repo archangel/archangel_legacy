7dfa882fa9f9c4fd055a8a3f89465d9c5e2ac686
David Freerksen <dfreerksen@gmail.com>
2019-05-27 12:45:09 -0700

    Rubocop cleanup

--- lib/archangel/testing_support/support/factory_bot.rb
+++ lib/archangel/testing_support/support/factory_bot.rb
@@ -6,12 +6,10 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
-    begin
-      DatabaseCleaner.start
+    DatabaseCleaner.start
 
-      FactoryBot.lint traits: true
-    ensure
-      DatabaseCleaner.clean
-    end
+    FactoryBot.lint traits: true
+  ensure
+    DatabaseCleaner.clean
   end
 end
