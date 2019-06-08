a7168d8c639f6c099c30739b52e448716baa0f19
David Freerksen <dfreerksen@gmail.com>
2019-06-07 20:03:44 -0700

    Clean up Backend Assets feature tests

--- spec/features/backend/assets/delete_spec.rb
+++ spec/features/backend/assets/delete_spec.rb
@@ -4,25 +4,28 @@ require "rails_helper"
 
 RSpec.describe "Backend - Assets (HTML)", type: :feature do
   describe "deletion" do
-    before { stub_authorization!(profile) }
+    before do
+      stub_authorization!
 
-    let(:profile) { create(:user) }
-
-    scenario "confirms the Asset is no longer accessible" do
       create(:asset, file_name: "delete-me.jpg")
       create(:asset, file_name: "keep-me.jpg")
+    end
 
+    it "shows a success message to confirm the Asset is no longer available" do
       visit "/backend/assets"
 
-      within("tbody tr:eq(1)") do
-        click_on "Destroy"
-      end
+      within("tbody tr:eq(1)") { click_on "Destroy" }
 
       expect(page).to have_content("Asset was successfully destroyed.")
+    end
+
+    it "does not list Asset after it is deleted" do
+      visit "/backend/assets"
+
+      within("tbody tr:eq(1)") { click_on "Destroy" }
 
       within("tbody tr:eq(1)") do
         expect(page).not_to have_content("delete-me.jpg")
-        expect(page).to have_content("keep-me.jpg")
       end
     end
   end
--- spec/features/backend/assets/listing_spec.rb
+++ spec/features/backend/assets/listing_spec.rb
@@ -4,72 +4,74 @@ require "rails_helper"
 
 RSpec.describe "Backend - Assets (HTML)", type: :feature do
   describe "listing" do
-    before { stub_authorization!(profile) }
-
-    let(:profile) { create(:user) }
-
     before do
+      stub_authorization!
+
       ("A".."Z").each do |letter|
         create(:asset, file_name: "asset-#{letter}.jpg")
       end
     end
 
-    describe "sorted" do
-      scenario "ascending from A-Z (default)" do
+    describe "ascending from A-Z (default)" do
+      it "displays the first resource" do
         visit "/backend/assets"
 
         within("tbody") do
           expect(page.find("tr:eq(1)")).to have_content("asset-A.jpg")
-          expect(page.find("tr:eq(1)"))
-            .to have_css("img[src^='/uploads/archangel/asset/file']")
+        end
+      end
 
+      it "displays the second resource" do
+        visit "/backend/assets"
+
+        within("tbody") do
           expect(page.find("tr:eq(2)")).to have_content("asset-B.jpg")
-          expect(page.find("tr:eq(2)"))
-            .to have_css("img[src^='/uploads/archangel/asset/file']")
+        end
+      end
 
+      it "displays the third resource" do
+        visit "/backend/assets"
+
+        within("tbody") do
           expect(page.find("tr:eq(3)")).to have_content("asset-C.jpg")
-          expect(page.find("tr:eq(3)"))
-            .to have_css("img[src^='/uploads/archangel/asset/file']")
         end
       end
     end
 
     describe "paginated" do
-      scenario "finds the second page of Assets" do
+      it "does not find the first page of Assets" do
         visit "/backend/assets?page=2"
 
         expect(page).not_to have_content("asset-A.jpg")
-        expect(page).not_to have_content("asset-X.jpg")
-
-        expect(page).to have_content("asset-Y.jpg")
-        expect(page).to have_content("asset-Z.jpg")
       end
 
-      scenario "finds the second page of Assets with `per` count" do
+      it "finds the second page of Assets" do
+        visit "/backend/assets?page=2"
+
+        expect(page).to have_content("asset-Y.jpg")
+      end
+
+      it "does not find the first page of Assets with `per` count" do
         visit "/backend/assets?page=2&per=3"
 
         expect(page).not_to have_content("asset-A.jpg")
-        expect(page).not_to have_content("asset-C.jpg")
-
-        expect(page).to have_content("asset-D.jpg")
-        expect(page).to have_content("asset-F.jpg")
-
-        expect(page).not_to have_content("asset-G.jpg")
-        expect(page).not_to have_content("asset-Z.jpg")
       end
 
-      scenario "finds nothing outside the count" do
+      it "finds the second page of Assets with `per` count" do
+        visit "/backend/assets?page=2&per=3"
+
+        expect(page).to have_content("asset-D.jpg")
+      end
+
+      it "finds nothing outside the count" do
         visit "/backend/assets?page=2&per=26"
 
         expect(page).to have_content("No assets found.")
-
-        expect(page).not_to have_content("asset-A.jpg")
-        expect(page).not_to have_content("asset-Z.jpg")
       end
     end
 
     describe "excludes" do
-      scenario "deleted Assets" do
+      it "does not list deleted Asset after it is deleted" do
         create(:asset, :deleted, file_name: "deleted.jpg")
 
         visit "/backend/assets"
