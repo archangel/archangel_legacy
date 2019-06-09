068f859596c3c910778681f52a9194779cb0fd04
David Freerksen <dfreerksen@gmail.com>
2019-06-08 19:00:16 -0700

    Convert link_to Liquid filter to feature tests

--- spec/features/frontend/liquid/filters/link_to_spec.rb
+++ spec/features/frontend/liquid/filters/link_to_spec.rb
@@ -4,64 +4,46 @@ require "rails_helper"
 
 RSpec.describe "Liquid custom filters", type: :feature do
   describe "for `link_to` filter" do
-    it "builds linked text with URL" do
-      content = "{{ 'Hello world' | link_to: 'https://github.com' }}"
-
-      create(:page, slug: "amazing", content: content)
-
-      visit "/amazing"
-
-      expect(page).to have_link("Hello world", href: "https://github.com")
-    end
-
-    it "builds linked text with path" do
-      content = "{{ 'Hello world' | link_to: '/some/place' }}"
-
-      create(:page, slug: "amazing", content: content)
-
-      visit "/amazing"
-
-      expect(page).to have_link("Hello world", href: "/some/place")
-    end
+    let(:repo) { "https://github.com/archangel/archangel" }
 
     it "returns plain text with blank link value" do
-      content = "{{ 'Hello world' | link_to: '' }}"
+      content = "{{ 'Amazing Grace' | link_to }}"
 
       create(:page, slug: "amazing", content: content)
 
       visit "/amazing"
 
-      expect(page).to have_text("Hello world")
+      expect(page).to have_text("Amazing Grace")
     end
 
-    it "returns plain text without a link with blank link value" do
-      content = "{{ 'Hello world' | link_to: '' }}"
+    it "returns plain text with empty link value" do
+      content = "{{ 'Amazing Grace' | link_to: '' }}"
 
       create(:page, slug: "amazing", content: content)
 
       visit "/amazing"
 
-      expect(page).not_to have_link("Hello world", href: "https://github.com")
+      expect(page).to have_text("Amazing Grace")
     end
 
-    it "returns plain text with `null` link value" do
-      content = "{{ 'Hello world' | link_to: null }}"
+    it "returns linked text with a link" do
+      content = "{{ 'Amazing Grace' | link_to: '#{repo}' }}"
 
       create(:page, slug: "amazing", content: content)
 
       visit "/amazing"
 
-      expect(page).to have_text("Hello world")
+      expect(page).to have_link("Amazing Grace", href: repo)
     end
 
-    it "returns plain text without a link with `null` link value" do
-      content = "{{ 'Hello world' | link_to: null }}"
+    it "returns linked text with a path" do
+      content = "{{ 'Amazing Grace' | link_to: '/amazing/grace' }}"
 
       create(:page, slug: "amazing", content: content)
 
       visit "/amazing"
 
-      expect(page).not_to have_link("Hello world", href: "https://github.com")
+      expect(page).to have_link("Amazing Grace", href: "/amazing/grace")
     end
   end
 end
