a3694ce6a88e33fe9d0a14fd894966111297ed7b
David Freerksen <dfreerksen@gmail.com>
2019-07-21 13:57:39 -0700

    General Javascript

--- app/themes/default/assets/javascripts/default/backend/archangel.js
+++ app/themes/default/assets/javascripts/default/backend/archangel.js
@@ -4,21 +4,22 @@ $(document).ready(function () {
     $('body').toggleClass('body-small', ($(window).width() <= 768));
   }).trigger('resize');
 
+  $(document).on('scroll', function() {
+    var scrollDistance = $(this).scrollTop();
+
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  $('a.scroll-to-top').click(function(e) {
+    $('html, body').scrollTop(0);
+
+    e.preventDefault();
+  });
+
   $('[data-toggle=popover]').popover();
 
-  if (localStorageSupport()) {
-    var collapse = localStorage.getItem('collapse_menu');
-    var body = $('body');
-
-    if (collapse == 'on') {
-      if ( ! body.hasClass('body-small')) {
-        body.addClass('mini-navbar');
-      }
-    }
-  }
-
 });
-
-function localStorageSupport() {
-  return (('localStorage' in window) && window['localStorage'] !== null);
-}
