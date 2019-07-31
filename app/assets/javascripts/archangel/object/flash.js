a0af7a2255c9f9fd56d46ffdea98e57e4ce865cc
David Freerksen <dfreerksen@gmail.com>
2019-07-30 20:57:02 -0700

    Display the actual flash message

--- app/assets/javascripts/archangel/object/flash.js
+++ app/assets/javascripts/archangel/object/flash.js
@@ -36,7 +36,7 @@
 
       $('.alert-messages').append(
         '<div class="alert alert-' + type + ' alert-link alert-dismissable" role="alert">' +
-          'Sort order has been updated' +
+          message +
           '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
             '<span aria-hidden="true">&times;</span>' +
           '</button>' +
