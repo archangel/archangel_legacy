4d997eb9c971bc8ac4f52e243a56063ac0a11683
David Freerksen <dfreerksen@gmail.com>
2019-08-09 09:04:52 -0700

    Move schema files under fixtures

--- spec/support/matchers/match_response_schema.rb
+++ spec/support/matchers/match_response_schema.rb
@@ -4,7 +4,7 @@ require "json-schema"
 
 RSpec::Matchers.define :match_response_schema do |schema|
   match do |response|
-    schema_directory = "#{Dir.pwd}/spec/support/schemas"
+    schema_directory = "#{Dir.pwd}/spec/fixtures/schemas"
     schema_path = "#{schema_directory}/#{schema}.json"
 
     JSON::Validator.validate!(schema_path, response.body, strict: true)
